378751d8c320209247dd0a606c786e09283b3d05
snayrouz <samuel.nayrouz@gmail.com>
2017-09-07 08:36:26 -0600

    Add psuedo to user show page

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ git_source(:github) do |repo_name|
 end
 
 gem 'bootstrap', '~> 4.0.0.alpha6'
-
+gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.3'
 # Use postgresql as the database for Active Record
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,5 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -25,7 +21,7 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
