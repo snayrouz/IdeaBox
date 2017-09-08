42539d9c68d424beb3569fb327df7b8071b20534
snayrouz <samuel.nayrouz@gmail.com>
2017-09-08 14:13:25 -0600

    Check tests

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -7,6 +7,10 @@ class Admin::CategoriesController < Admin::BaseController
     @category = Category.new
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
   def create
     @category = Category.new(category_params)
     if @category.save
@@ -22,10 +26,21 @@ class Admin::CategoriesController < Admin::BaseController
     @category = Category.find(params[:id])
   end
 
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      #flash successful update
+      redirect_to admin_categories_path
+    else
+      #flash unsuccessful message
+      redirect_to edit_admin_category_path
+    end
+  end
+
   def destroy
     @category = Category.find(params[:id])
     @category.destroy
-
+    #flash category deleted
     redirect_to admin_categories_path
   end
 
--- spec/models/admin_categories_spec.rb
+++ spec/models/admin_categories_spec.rb
@@ -17,8 +17,8 @@ describe "User visits categories new page" do
       fill_in "category[name]", with: "Lunch"
       click_on "Create Category"
 
-      expect(current_path).to eq(admin_categories_path)
       expect(page).to have_content("Lunch")
+      expect(current_path).to eq(admin_categories_path)
     end
 
     it "views all caterogies on the index page" do
@@ -31,7 +31,7 @@ describe "User visits categories new page" do
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
       visit admin_categories_path
-      
+
       expect(page).to have_content(category.name)
       expect(page).to have_content(category_2.name)
     end
