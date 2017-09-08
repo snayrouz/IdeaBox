fd5079c67754f28249078786815dbeef8ab1d47d
snayrouz <samuel.nayrouz@gmail.com>
2017-09-08 13:21:44 -0600

    Add admin cateogies passing tests

--- spec/models/admin_categories_spec.rb
+++ spec/models/admin_categories_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "User visits categories new page" do
   context "as an administrator" do
-    it "allows admin to make new category" do
+    it "allows admin to view page and add new category " do
       admin = User.create(username: "samnay",
                           email: "sn@aol.com",
                           password: "password",
@@ -11,8 +11,29 @@ describe "User visits categories new page" do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
       visit new_admin_category_path
-      
+
       expect(page).to have_content("New Category")
+
+      fill_in "category[name]", with: "Lunch"
+      click_on "Create Category"
+
+      expect(current_path).to eq(admin_categories_path)
+      expect(page).to have_content("Lunch")
+    end
+
+    it "views all caterogies on the index page" do
+      category = Category.create(name: "Lunch")
+      category_2 = Category.create(name: "Fun")
+      admin = User.create(username: "samnay",
+                          email: "sn@aol.com",
+                          password: "password",
+                          role: 1)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+      visit admin_categories_path
+      
+      expect(page).to have_content(category.name)
+      expect(page).to have_content(category_2.name)
     end
   end
 
