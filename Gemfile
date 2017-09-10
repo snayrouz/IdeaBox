cff6ababd0348b8c78fda425d415a62e7ee89661
snayrouz <samuel.nayrouz@gmail.com>
2017-09-09 23:12:24 -0600

    Implement faker gem in seed file and seed db

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ end
 gem 'bootstrap', '~> 4.0.0.alpha6'
 gem "paperclip", "~> 5.0.0"
 gem 'devise'
+gem 'faker'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.3'
 # Use postgresql as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,35 @@
-# count = 0
-# category = Category.create(name: Faker::File.extension)
-# idea = Idea.create(title: Faker::GameOfThrones)
-#
-# Idea.destroy_all
-# Image.destroy_all
-# Category.destroy_all
-# User.destroy_all
-#
-# # USERS = ["Sam", "Darby", "Moose", "Peter", "Jerry"]
-# # IDEAS = ["Concert", "Curriculum", "Vacation", "Holidays", "Dog"]
-# # IMAGES = ["https://robohash.org/1","https://robohash.org/2","https://robohash.org/3","https://robohash.org/4","https://robohash.org/5"]
-# # CATEGORIES = ["Travel", "Pets", "Personal", "Turing", "Music", "Fun"]
-# # DESCRIPTIONS = ["Fleet Foxes at Red Rocks", "Mod 2 CRUDing many to many", "Europe for post school vacation", "go to Cali for Xmas", "Get moose leash trained"]
-# # EMAILS = ["sam@example.com", "darby@example.com", "moose@example.com", "peter@example.com", "jerry@example.com", "random@email.com"]
-#
-# USERS.each do |name|
-#   user = User.create!(username: name, password: IDEAS.sample, email: EMAILS.sample)
-#   puts "Created #{user.username}"
-#   5.times do |num|
-#     user.ideas.create!(title: IDEAS.sample, description: DESCRIPTIONS.sample, category: Category.find_or_create_by(name: CATEGORIES.sample))
-#   end
-# end
-#
-# IMAGES.each do |image|
-#   counter = rand(100)
-#   Image.create!(title: "Robot #{counter}", url: image)
-# end
-#
-# User.create(username: "user", password: "123", email: 'user@example.com')
-# User.create(username: "admin", password: "123", email: 'admin@example.com', role: 1)
-#
-# puts "You did it. You finally seeded this database. Go ahead and crack open a cold beer!"
+Idea.destroy_all
+Image.destroy_all
+Category.destroy_all
+User.destroy_all
+
+10.times do
+  Category.create(name: Faker::Job.key_skill)
+end
+puts "Seeded categories"
+
+20.times do
+  user = User.create!(username: Faker::Internet.user_name,
+                password: Faker::Internet.password,
+                email: Faker::Internet.email,
+                role: Faker::Number.between(0, 1))
+  20.times do
+    user.ideas.create!(title: Faker::RickAndMorty.quote,
+                       description: Faker::FamilyGuy.quote,
+                       category_id: Faker::Number.between(1, 10),
+                       user_id: Faker::Number.between(1, 20))
+  end
+end
+puts "Seeded users and ideas"
+
+20.times do
+  counter = rand(20)
+  Image.create!(title: "Robot #{counter}", url: Faker::Fillmurray.image)
+end
+
+puts "Seeded images"
+
+User.create(username: "user", password: "123", email: 'user@example.com')
+User.create(username: "admin", password: "123", email: 'admin@example.com', role: 1)
+
+puts "You did it. You finally seeded this database. Go ahead and crack open a cold beer!"
