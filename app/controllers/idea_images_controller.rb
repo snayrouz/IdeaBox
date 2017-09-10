5f24a407c2f92928a1f6e7197c23b7ea21149374
snayrouz <samuel.nayrouz@gmail.com>
2017-09-09 22:08:59 -0600

    Add nav bar partials

--- app/controllers/idea_images_controller.rb
+++ app/controllers/idea_images_controller.rb
@@ -1,3 +1,45 @@
 class IdeaImages < ApplicationController
+  def index
+  end
+
+  def new
+    @idea = current_user.ideas.find(params[:idea_id])
+    @idea_image = @idea.idea_images.new
+  end
+
+  def create
+    # @user = current_user
+    # @idea = @user.ideas.find(params[:idea_id])
+    # @idea = Idea.find(params[:idea_id])
+    # IdeaImage.create(idea_image_params)
+    # @user = current_user
+    # @idea = @user.ideas.find(params[:idea_id])
+    # @image = @idea.images.new(idea_image_params)
+    # if @image.save
+    #   flash[:success] = "Image Added!"
+    #   redirect_to user_idea_path(@user, @idea)
+    # else
+    #   flash[:error] = "Error #{@image.errors.keys} #{@image.errors.values}"
+    #   render :new
+    # end
+  end
+
+  def delete
+    @user = current_user
+    @idea = @user.ideas.find(params[:idea_id])
+    @image = @idea.images.find(params[:image_id])
+    if @image.destroy
+      flash[:success] = "Image destroyed!"
+      redirect_to user_idea_path(@user, @idea)
+    else
+    flash[:errror] = "Error, #{@image.errors.keys}, #{@image.errors.values}"
+    end
+  end
+
+  private
+
+  def idea_image_params
+    params.require(:idea_image).permit(:id, :idea_id)
+  end
 
 end
